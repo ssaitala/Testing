7de873f79185456e553fbde73e9bc9734372f434
Srinivas Saitala <UC210302@corp.ositax.com>
2016-06-01 12:46:42 -0700

    first commit

---
+++ src/Score.java
@@ -0,0 +1,21 @@
+
+public class Score {
+	private int score;
+	private String name;
+	
+	public Score(int score, String name)
+	{
+		this.score = score;
+		this.name = name;
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+	
+	public String name()
+	{
+		return name;
+	}
+}
